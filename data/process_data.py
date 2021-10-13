b9ad312221423611e208d5263382b78c5576c273
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2021-10-13 11:56:02 -0300

    Update process_data and train_classifier

--- data/process_data.py
+++ data/process_data.py
@@ -3,7 +3,16 @@ import pandas as pd
 import sqlalchemy
 
 
-def load_data(messages_filepath, categories_filepath):
+def load_data(messages_filepath: str, categories_filepath: str) -> pd.DataFrame:
+    """
+    Loads the data from the given messages and categories file path.
+
+    Args:
+        messages_filepath (str): File path of the messages to load.
+        categories_filepath (str): File path of the categories to load.
+    Returns:
+        df (pd.DataFrame): Dataframe of the merged Messages and Categories files data.
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
 
@@ -11,7 +20,16 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 
-def process_categories(df):
+def process_categories(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Process, clean and merge the categories dataset into the main one.
+
+    Args:
+        df (pd.DataFrame): Dataframe of the merged Messages and Categories files
+
+    Returns:
+        df (pd.DataFrame): Dataframe cleaned and processed by categories
+    """
     categories = df['categories'].str.split(';', expand=True)
     row = categories.iloc[0]
     category_colnames = row.apply(lambda x: x[:-2]).values
@@ -21,25 +39,47 @@ def process_categories(df):
         categories[column] = categories[column].apply(lambda x: 1 if '1' in x else 0)
 
     df.drop('categories', inplace=True, axis=1)
-
     df = pd.concat([df, categories], axis=1)
-
     return df
 
 
-def clean_data(df):
-    df_clean_categories = process_categories(df)
-    df_clean_categories.drop_duplicates(inplace=True)
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function responsible for cleaning the data.
 
-    return df
+    Args:
+        df (pd.DataFrame): DataFrame to be cleaned.
+    Returns:
+        df_cleaned (pd.DataFrame): Cleaned DataFrame.
+    """
+    df_cleaned = process_categories(df)
+    df_cleaned.drop_duplicates(inplace=True)
+
+    return df_cleaned
 
 
-def save_data(df, database_filename):
-    engine = sqlalchemy.create_engine(f'sqlite:///{database_filename}.db')
+def save_data(df: pd.DataFrame, database_filename: str) -> None:
+    """
+    Save the Dataframe passed to a sqlite database.
+
+    Args:
+        df (pd.DataFrame): DataFrame to be saved.
+        database_filename (str): Filename of the database.
+    """
+    engine = sqlalchemy.create_engine(f'sqlite:///{database_filename}')
     df.to_sql('disaster_messages', engine, index=False)
 
 
 def main():
+    """
+    Main function responsible to handle all the ETL pipeline process.
+    Extract:
+        load_data()
+    Transform:
+        clean_data()
+    Load:
+        save_data()
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,39 +1,147 @@
+"""Module responsible for training the Classifier."""
+import pickle
+import re
 import sys
+from typing import List
+
+import nltk
+import numpy as np
+import pandas as pd
+import sqlalchemy
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+
+nltk.download(['punkt', 'stopwords', 'wordnet'])
 
 
-def load_data(database_filepath):
-    pass
+def load_data(database_filepath: str):
+    """
+    Load database from the database_filepath.
+
+    Args:
+        database_filepath (str): Filepath of the database.
+
+    Returns:
+        X (numpy.ndarray[str]): Array of disaster messages
+        y (pd.DataFrame): DF with all categories and its 0 and 1 values
+        category_names (list[str]): List of all the categories columns
+    """
+    engine = sqlalchemy.create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('disaster_messages', engine)
+    X = df.message.values
+    y = df.loc[::, 'related':]
+    category_names = y.columns
+
+    return X, y, category_names
 
 
-def tokenize(text):
-    pass
+def tokenize(text: str) -> List[str]:
+    """
+    Clean, normalize, tokenize and process text parameter.
+
+    Args:
+        text (str): String phrase with text.
+
+    Returns:
+        stemmed_tokens (list[str]): Cleaned text
+    """
+    # set to lower case
+    text = text.lower()
+
+    # remove punctuation
+    text = re.sub(r'[^\w\s]', '', text)
+
+    # replace double empty spaces to one
+    text = text.replace('  ', ' ').strip()
+
+    # tokenize the text
+    tokens = word_tokenize(text)
+
+    # remove stop words
+    cleaned_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    # lemmatize the tokens
+    lemmatized_tokens = [WordNetLemmatizer().lemmatize(token) for token in cleaned_tokens]
+
+    # stemming the tokens
+    stemmed_tokens = [PorterStemmer().stem(token) for token in lemmatized_tokens]
+
+    return stemmed_tokens
 
 
-def build_model():
-    pass
+def build_model() -> Pipeline:
+    """
+    Build pipeline model.
+
+    Uses CountVectorizer, TfidfTransformer and a
+    MultiOutputClassifier with RandomForestClassifier.
+
+    Returns:
+        pipeline (sklearn.Pipeline): Pipeline model.
+    """
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model: Pipeline, X_test: np.ndarray,
+                   Y_test: pd.DataFrame, category_names: str):
+    """
+    Evaluate and prints the model precision, recall and F1 score.
+
+    Args:
+        model (sklearn.Pipeline): Pipeline model.
+        X_test (numpy.ndarray): Test data
+        Y_test (pd.DataFrame): Df for testing
+        category_names (List[str]): List of all the categories columns
+    """
+    y_pred = model.predict(X_test)
+    # classification report
+    print(classification_report(
+        Y_test,
+        y_pred,
+        target_names=category_names
+    ))
 
 
-def save_model(model, model_filepath):
-    pass
+def save_model(model: Pipeline, model_filepath: str):
+    """
+    Save the model on a pickle file.
+
+    Args:
+        model (sklearn.Pipeline): Pipeline model.
+        model_filepath (str): Filepath to save the pkl file
+    """
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def main():
+    """Main function to process the classifier logic."""
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -43,9 +151,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
