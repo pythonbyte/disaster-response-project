bb047a42f5722f7b7d14dc587c15965bec5cfe95
Eduardo Chiarotti <dudumelgaco@hotmail.com>
2021-10-13 11:55:15 -0300

    Update run file to add more graphs

--- app/run.py
+++ app/run.py
@@ -2,6 +2,7 @@ import json
 import plotly
 import pandas as pd
 import joblib
+from collections import Counter
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -14,7 +15,9 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+
 def tokenize(text):
+    """Function to tokenize text."""
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -25,32 +28,94 @@ def tokenize(text):
 
     return clean_tokens
 
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+database_filepath = '../data/DisasterResponse.db'
+engine = create_engine(f'sqlite:///{database_filepath}')
+df = pd.read_sql_table('disaster_messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+
+    categories_counts = df.loc[::, 'related':].sum().sort_values(ascending=False)
+    categories_names = [i.replace('_', ' ').capitalize() for i in categories_counts.keys()]
+
+    # Logic to return most common words, but takes too much time so
+    # I'll pass direct values and I manually filtered the most used ones
+    # df['tokens'] = df['message'].apply(lambda x: tokenize(x))
+    # results = Counter()
+    # df['tokens'].apply(results.update)
+    # sorted(results.items(), key=lambda x: -x[1])
+
+    results = [('peopl', 2999),
+               ('water', 2943),
+               ('food', 2811),
+               ('help', 2810),
+               ('need', 2747),
+               ('pleas', 1954),
+               ('earthquak', 1868),
+               ('area', 1655),
+               ('flood', 1336),
+               ('countri', 1220),
+               ('thank', 1112),
+               ('govern', 1074),
+               ('hous', 1040),
+               ('inform', 1028),
+               ('rain', 1017)]
+
     graphs = [
         {
             'data': [
                 Bar(
+                    x=[i[0] for i in results],
+                    y=[i[1] for i in results]
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Most Used Words (Normalized)',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Word text",
+                    'tickangle': -40,
+                },
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=categories_names,
+                    y=categories_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category Name",
+                    'tickangle': -40,
+                },
+            }
+        },
+        {
+            'data': [
+                Bar(
                     x=genre_names,
-                    y=genre_counts
+                    y=genre_counts,
+                    marker_color=['steelblue', 'firebrick', 'green']
                 )
             ],
 
@@ -65,11 +130,11 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +143,15 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    classification_results = dict(
+        sorted(classification_results.items(), key=lambda item: -item[1])
+    )
     return render_template(
         'go.html',
         query=query,
